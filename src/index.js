d033d2db5ca40bcf65d477822cc249fb7e32c295
HarshK-12 <khandelwalharshh@gmail.com>
2023-03-11 17:13:26 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,68 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Index from './redux/Index';
+
+// import Fetchdata from './Fetchdata/Fetchdata.js'
+// import RandomData from './RandomData/RandomData.js';
+// import  Tesla from './Tesla/Tesla.js';
+// import Axios from './axios/axios.js'
+// import Movie from './MovieDb/Movie.js';
+// import Todo from './Todo/Todo.js';
+// import Netflix from './Netflix/Netflix.js'
+// import Game from './Game/Game.js'
+// import Router from './Router/Router.js'
+// import App from './Routing/App.js'
+// import Library from './Library/Library.js'
+import Page1 from './CoordinateChecker/page1';
+// import UseContext from './custom hooks/markdown-files/Contextapi/UseContext';
+// import App from './shoppingCart/App';
+// import Flag from './Flag/Flag';
+// import UseContext from './Contextapi/UseContext'
+// import Tempapp from './TempratureConvertor/Tempapp';
+// import ContextApp from './Tempreature converter/ContextApp';
+// import Counter from './Reducer/Counter';
+  // import SignUpForm from './Form-reducer/SignUpForm';
+// import Fom1 from './Form/Fom1';
+// import Home from './Shopping List/Home';
+
+
+// import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+  <>
+
+{/* <Home/> */}
+  {/* <Index /> */}
+  {/* <React.StrictMode> */}
+  {/* <App/> */}
+{/* <Tesla/> */}
+{/* <RandomData/> */}
+{/* <Axios/> */}
+{/* <Fetchdata/> */}
+ {/* <Movie/> */}
+ {/* <Todo/> */}
+ {/* <Netflix/> */}
+ {/* <Game/> */}
+ <Page1/>
+{/* <App/> */}
+{/* <Router/> */}
+ {/* <Library/> */}
+{/* <Flag/> */}
+{/* <UseContext/> */}
+{/* <Tempapp/> */}
+{/* <ContextApp/> */}
+{/* <Counter/> */}
+{/* <SignUpForm/> */}
+{/* <Fom1/> */}
+  {/* </React.StrictMode> */}
+  </>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
